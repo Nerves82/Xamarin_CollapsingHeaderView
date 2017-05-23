72a9f8d5d1b4e8360e65c0acba0d3de2b1e9ff86
Sevren Brewer <nervousmotions@icloud.com>
2017-05-23 16:23:56 -0700

    Adding a fade in view. Making sure animated views are working correctly

--- CollapsingHeaderView.cs
+++ CollapsingHeaderView.cs
@@ -1,4 +1,3 @@
-
 using Foundation;
 using System;
 using UIKit;
@@ -18,15 +17,15 @@ namespace Project.iOS
 	//Enumeration of attributes that can be transformed when scrolling.
 	public enum MGAttribute
 	{
-		MGAttributeX = 1,
-		MGAttributeY,
-		MGAttributeWidth,
-		MGAttributeHeight,
-		MGAttributeAlpha,
-		MGAttributeCornerRadius,
-		MGAttributeShadowRadius,
-		MGAttributeShadowOpacity,
-		MGAttributeFontSize
+		X = 1,
+		Y,
+		Width,
+		Height,
+		Alpha,
+		CornerRadius,
+		ShadowRadius,
+		ShadowOpacity,
+		FontSize
 	}
 
 	//Defines an attribute to be transformed when scrolling.
@@ -36,7 +35,7 @@ namespace Project.iOS
 		public MGTransformCurve Curve;
 		public float Value, OrigValue;
 
-		public MGTransform TransformAttribute(MGAttribute attr, float val)
+		public static MGTransform TransformAttribute(MGAttribute attr, float val)
 		{
 			MGTransform a = new MGTransform();
 			a.Attribute = attr;
@@ -49,20 +48,19 @@ namespace Project.iOS
 
 	public partial class CollapsingHeaderView : UIView
 	{
-		float MinimumHeaderHeight { get; set; }
 		public bool AlwaysCollapse { get; set; }
 
-		List<NSLayoutConstraint> hdrConstrs;
-		List<nfloat> hdrConstrVals;
-		List<UIView> transfViews, fadeViews;
-		Dictionary<string, List<MGTransform>> transfAttrs;
-		Dictionary<string, nfloat> alphaRatios;
-		Dictionary<string, Dictionary<string, nfloat>> constrVals;
-		Dictionary<string, Dictionary<string, NSLayoutConstraint>> constrs;
-		Dictionary<NSLayoutAttribute, bool> vertConstraints;
-		nfloat lastOffset;
-		nfloat header_ht, scroll_ht, offset_max;
-		UIFont font;
+		List<NSLayoutConstraint> _headerConstraints;
+		List<nfloat> _headerConstraintValues;
+		List<UIView> _transfViews, _fadeOutViews, _fadeInViews;
+		Dictionary<string, List<MGTransform>> _transfAttrs;
+		Dictionary<string, nfloat> _alphaRatios;
+		Dictionary<string, Dictionary<string, nfloat>> _constrVals;
+		Dictionary<string, Dictionary<string, NSLayoutConstraint>> _constrs;
+		Dictionary<NSLayoutAttribute, bool> _vertConstraints;
+		nfloat _lastOffset;
+		nfloat _headerHeight, _scrollHeight, _maxOffset;
+		float _minimumHeaderHeight;
 
 		public CollapsingHeaderView()
 		{
@@ -91,20 +89,21 @@ namespace Project.iOS
 
 		private void InitView()
 		{
-			transfViews = new List<UIView>();
-			fadeViews = new List<UIView>();
-			transfAttrs = new Dictionary<string, List<MGTransform>>();
-			constrs = new Dictionary<string, Dictionary<string, NSLayoutConstraint>>();
-			constrVals = new Dictionary<string, Dictionary<string, nfloat>>();
-			alphaRatios = new Dictionary<string, nfloat>();
-			vertConstraints = new Dictionary<NSLayoutAttribute, bool>();
-			vertConstraints.Add(NSLayoutAttribute.Top, true);
-			vertConstraints.Add(NSLayoutAttribute.TopMargin, true);
-			vertConstraints.Add(NSLayoutAttribute.Bottom, true);
-			vertConstraints.Add(NSLayoutAttribute.BottomMargin, true);
+			_transfViews = new List<UIView>();
+			_fadeOutViews = new List<UIView>();
+			_fadeInViews = new List<UIView>();
+			_transfAttrs = new Dictionary<string, List<MGTransform>>();
+			_constrs = new Dictionary<string, Dictionary<string, NSLayoutConstraint>>();
+			_constrVals = new Dictionary<string, Dictionary<string, nfloat>>();
+			_alphaRatios = new Dictionary<string, nfloat>();
+			_vertConstraints = new Dictionary<NSLayoutAttribute, bool>();
+			_vertConstraints.Add(NSLayoutAttribute.Top, true);
+			_vertConstraints.Add(NSLayoutAttribute.TopMargin, true);
+			_vertConstraints.Add(NSLayoutAttribute.Bottom, true);
+			_vertConstraints.Add(NSLayoutAttribute.BottomMargin, true);
 
-			header_ht = Frame.Size.Height;
-			scroll_ht = -1.0f;
+			_headerHeight = Frame.Size.Height;
+			_scrollHeight = -1.0f;
 
 			SetMinimumHeaderHeight(60);
 			AlwaysCollapse = true;
@@ -112,8 +111,8 @@ namespace Project.iOS
 
 		public void SetMinimumHeaderHeight(float minimumHeaderHeight)
 		{
-			MinimumHeaderHeight = minimumHeaderHeight;
-			offset_max = header_ht - MinimumHeaderHeight;
+			_minimumHeaderHeight = minimumHeaderHeight;
+			_maxOffset = _headerHeight - _minimumHeaderHeight;
 		}
 
 		public void SetCollapsingConstraint(NSLayoutConstraint c)
@@ -123,109 +122,134 @@ namespace Project.iOS
 
 		private void SetCollapsingConstraints(List<NSLayoutConstraint> cs)
 		{
-			hdrConstrs = cs;
+			_headerConstraints = cs;
 			var vals = new List<nfloat>();
 
 			foreach (var c in cs) {
-
 				vals.Add(c.Constant);
 			}
-			hdrConstrVals = vals;
+			_headerConstraintValues = vals;
 		}
 
 		public void CollapseWithScroll(UIScrollView scrollView)
 		{
-			nfloat dy = scrollView.ContentOffset.Y;
-			if (scroll_ht < 0.0f) {
-				scroll_ht = scrollView.Frame.Size.Height;
+			nfloat y = scrollView.ContentOffset.Y;
+			if (_scrollHeight < 0.0f) {
+				_scrollHeight = scrollView.Frame.Size.Height;
 			}
-			nfloat scrollableHeight = scrollView.ContentSize.Height - scroll_ht;
+			nfloat scrollableHeight = scrollView.ContentSize.Height - _scrollHeight;
 
-			if (scrollableHeight / 2.0 < offset_max) {
+			if (scrollableHeight / 2.0 < _maxOffset) {
 				if (AlwaysCollapse == true) {
 					UIEdgeInsets scrInset = scrollView.ContentInset;
-					scrInset.Bottom = 2.0f * offset_max - scrollableHeight;
+					scrInset.Bottom = 2.0f * _maxOffset - scrollableHeight;
 					scrollView.ContentInset = scrInset;
 				} else {
 					return;
 				}
 			}
 
-			if (dy > 0.0f) {
-				if (header_ht - dy > MinimumHeaderHeight) {
-					ScrollHeaderToOffset(dy);
-				} else if (header_ht - lastOffset > MinimumHeaderHeight) {
-					ScrollHeaderToOffset(offset_max);
+			if (y > 0.0f) {
+				if (_headerHeight - y > _minimumHeaderHeight) {
+					ScrollHeaderToOffset(y);
+				} else if (_headerHeight - _lastOffset > _minimumHeaderHeight) {
+					ScrollHeaderToOffset(_maxOffset);
 				}
-			} else if (lastOffset > 0.0f) {
+			} else if (_lastOffset > 0.0f) {
 				ScrollHeaderToOffset(0.0f);
 			}
+
+			AnimateFadeInViews(y);
 
 			SetNeedsUpdateConstraints();
 			SetNeedsLayout();
 			LayoutIfNeeded();
 
-			lastOffset = dy;
+			_lastOffset = y;
 		}
 
-		private bool AddTransformingSubview(UIView view, List<MGTransform> transforms)
+		public bool AddTransformingSubview(UIView view, List<MGTransform> transforms)
 		{
 			var constrDict = new Dictionary<string, NSLayoutConstraint>();
 			var constrValDict = new Dictionary<string, nfloat>();
 
-			while (view != null) {
-				foreach (var constraints in view.Constraints) {
-					if (constraints.FirstItem == view) {
+			UIView v = view;
+			while (v != null) {
+				foreach (var constraint in v.Constraints) {
+					if (constraint.FirstItem == view) {
 
-						constrDict.Add(constraints.FirstAttribute.ToString(), constraints);
-						constrValDict.Add(constraints.FirstAttribute.ToString(), constraints.Constant);
+						if (constrDict.ContainsValue(constraint) == false) {
+							constrDict.Add(constraint.FirstAttribute.ToString(), constraint);
+							constrValDict.Add(constraint.FirstAttribute.ToString(), constraint.Constant);
+						}
 
-					} else if (constraints.SecondItem == view) {
+					} else if (constraint.SecondItem == view) {
 
-						constrDict.Add(constraints.SecondAttribute.ToString(), constraints);
-						constrValDict.Add(constraints.SecondAttribute.ToString(), constraints.Constant);
+						if (constrDict.ContainsValue(constraint) == false) {
+							constrDict.Add(constraint.SecondAttribute.ToString(), constraint);
+							constrValDict.Add(constraint.SecondAttribute.ToString(), constraint.Constant);
+						}
 					}
 				}
-				view = view.Superview;
+				v = v.Superview;
 			}
 
 			foreach (MGTransform transform in transforms) {
 				transform.OrigValue = (float)GetViewAttribute(transform.Attribute, view);
 			}
 
-			transfViews.Add(view);
-			transfAttrs.Add(view.GetHashCode().ToString(), transforms);
-			constrs.Add(view.GetHashCode().ToString(), constrDict);
-			constrVals.Add(view.GetHashCode().ToString(), constrValDict);
+			_transfViews.Add(view);
+			_transfAttrs.Add(view.GetHashCode().ToString(), transforms);
+			_constrs.Add(view.GetHashCode().ToString(), constrDict);
+			_constrVals.Add(view.GetHashCode().ToString(), constrValDict);
 
 			return true;
 		}
 
-		private bool AddFadingSubview(UIView view, nfloat ratio)
+		public bool AddFadeOutSubview(UIView view, nfloat ratio)
 		{
 			if (ratio < 0.0f || ratio > 1.0f) {
 				return false;
 			}
 
-			fadeViews.Add(view);
-			alphaRatios.Add(view.GetHashCode().ToString(), ratio);
+			_fadeOutViews.Add(view);
+			_alphaRatios.Add(view.GetHashCode().ToString(), ratio);
 
 			return true;
 		}
 
+		public void AddFadeInSubview(UIView view)
+		{
+			view.Alpha = 0f;
+			_fadeInViews.Add(view);
+		}
+
+		private void AnimateFadeInViews(nfloat offset)
+		{
+			foreach (var view in _fadeInViews) {
+
+				if (_headerHeight - offset > _minimumHeaderHeight) {
+					view.Alpha = 0f;
+				} else {
+					var a = (offset / 1000) * 3f;
+					view.Alpha = a;
+				}
+			}
+		}
+
 		private void ScrollHeaderToOffset(nfloat offset)
 		{
-			nfloat thisRatio = offset / offset_max;
+			nfloat thisRatio = offset / _maxOffset;
 
-			foreach (var view in fadeViews) {
-				nfloat alphaRatio = alphaRatios[view.GetHashCode().ToString()];
+			foreach (var view in _fadeOutViews) {
+				nfloat alphaRatio = _alphaRatios[view.GetHashCode().ToString()];
 				view.Alpha = -thisRatio / alphaRatio + 1;
 			}
 
-			foreach (var view in transfViews) {
-				Dictionary<string, NSLayoutConstraint> cs = constrs[view.GetHashCode().ToString()];
-				Dictionary<string, nfloat> cvs = constrVals[view.GetHashCode().ToString()];
-				List<MGTransform> attributeSet = transfAttrs[view.GetHashCode().ToString()];
+			foreach (var view in _transfViews) {
+				Dictionary<string, NSLayoutConstraint> cs = _constrs[view.GetHashCode().ToString()];
+				Dictionary<string, nfloat> cvs = _constrVals[view.GetHashCode().ToString()];
+				List<MGTransform> attributeSet = _transfAttrs[view.GetHashCode().ToString()];
 
 				foreach (var transform in attributeSet) {
 					SetAttribute(transform, view, thisRatio, cs, cvs);
@@ -236,8 +260,8 @@ namespace Project.iOS
 			hdrFrame.Y = -offset;
 			Frame = hdrFrame;
 
-			for (int i = 0; i < hdrConstrs.Count; i++) {
-				hdrConstrs[i].Constant = hdrConstrVals[i] - offset;
+			for (int i = 0; i < _headerConstraints.Count; i++) {
+				_headerConstraints[i].Constant = _headerConstraintValues[i] - offset;
 			}
 		}
 
@@ -268,23 +292,23 @@ namespace Project.iOS
 		private nfloat GetViewAttribute(MGAttribute attribute, UIView view)
 		{
 			switch (attribute) {
-				case MGAttribute.MGAttributeX:
+				case MGAttribute.X:
 					return view.Frame.X;
-				case MGAttribute.MGAttributeY:
+				case MGAttribute.Y:
 					return view.Frame.Y;
-				case MGAttribute.MGAttributeWidth:
+				case MGAttribute.Width:
 					return view.Frame.Size.Width;
-				case MGAttribute.MGAttributeHeight:
+				case MGAttribute.Height:
 					return view.Frame.Size.Height;
-				case MGAttribute.MGAttributeAlpha:
+				case MGAttribute.Alpha:
 					return view.Alpha;
-				case MGAttribute.MGAttributeCornerRadius:
+				case MGAttribute.CornerRadius:
 					return view.Layer.CornerRadius;
-				case MGAttribute.MGAttributeShadowOpacity:
+				case MGAttribute.ShadowOpacity:
 					return view.Layer.ShadowOpacity;
-				case MGAttribute.MGAttributeShadowRadius:
+				case MGAttribute.ShadowRadius:
 					return view.Layer.ShadowRadius;
-				case MGAttribute.MGAttributeFontSize:
+				case MGAttribute.FontSize:
 					if (view is UILabel) {
 						return ((UILabel)view).Font.PointSize;
 					} else if (view is UIButton) {
@@ -303,49 +327,69 @@ namespace Project.iOS
 		private void SetAttribute(MGTransform attr, UIView view, nfloat thisRatio, Dictionary<string, NSLayoutConstraint> cs, Dictionary<string, nfloat> cvals)
 		{
 			switch (attr.Attribute) {
-				case MGAttribute.MGAttributeX:
-					UpdateConstraint(cs[NSLayoutAttribute.Leading.ToString()], cvals[NSLayoutAttribute.Leading.ToString()], attr, thisRatio);
-					UpdateConstraint(cs[NSLayoutAttribute.LeadingMargin.ToString()], cvals[NSLayoutAttribute.LeadingMargin.ToString()], attr, thisRatio);
-					UpdateConstraint(cs[NSLayoutAttribute.Trailing.ToString()], cvals[NSLayoutAttribute.Trailing.ToString()], attr, thisRatio);
-					UpdateConstraint(cs[NSLayoutAttribute.TrailingMargin.ToString()], cvals[NSLayoutAttribute.TrailingMargin.ToString()], attr, thisRatio);
+				case MGAttribute.X:
+
+					if (cs.ContainsKey(NSLayoutAttribute.Leading.ToString()))
+						UpdateConstraint(cs[NSLayoutAttribute.Leading.ToString()], cvals[NSLayoutAttribute.Leading.ToString()], attr, thisRatio);
+
+					if (cs.ContainsKey(NSLayoutAttribute.LeadingMargin.ToString()))
+						UpdateConstraint(cs[NSLayoutAttribute.LeadingMargin.ToString()], cvals[NSLayoutAttribute.LeadingMargin.ToString()], attr, thisRatio);
+
+					if (cs.ContainsKey(NSLayoutAttribute.Trailing.ToString()))
+						UpdateConstraint(cs[NSLayoutAttribute.Trailing.ToString()], cvals[NSLayoutAttribute.Trailing.ToString()], attr, thisRatio);
+
+					if (cs.ContainsKey(NSLayoutAttribute.TrailingMargin.ToString()))
+						UpdateConstraint(cs[NSLayoutAttribute.TrailingMargin.ToString()], cvals[NSLayoutAttribute.TrailingMargin.ToString()], attr, thisRatio);
+
 					break;
-				case MGAttribute.MGAttributeY:
-					UpdateConstraint(cs[NSLayoutAttribute.Top.ToString()], cvals[NSLayoutAttribute.Top.ToString()], attr, thisRatio);
-					UpdateConstraint(cs[NSLayoutAttribute.TopMargin.ToString()], cvals[NSLayoutAttribute.TopMargin.ToString()], attr, thisRatio);
-					UpdateConstraint(cs[NSLayoutAttribute.Bottom.ToString()], cvals[NSLayoutAttribute.Bottom.ToString()], attr, thisRatio);
-					UpdateConstraint(cs[NSLayoutAttribute.BottomMargin.ToString()], cvals[NSLayoutAttribute.BottomMargin.ToString()], attr, thisRatio);
+				case MGAttribute.Y:
+
+					if (cs.ContainsKey(NSLayoutAttribute.Top.ToString()))
+						UpdateConstraint(cs[NSLayoutAttribute.Top.ToString()], cvals[NSLayoutAttribute.Top.ToString()], attr, thisRatio);
+
+					if (cs.ContainsKey(NSLayoutAttribute.TopMargin.ToString()))
+						UpdateConstraint(cs[NSLayoutAttribute.TopMargin.ToString()], cvals[NSLayoutAttribute.TopMargin.ToString()], attr, thisRatio);
+
+					if (cs.ContainsKey(NSLayoutAttribute.Bottom.ToString()))
+						UpdateConstraint(cs[NSLayoutAttribute.Bottom.ToString()], cvals[NSLayoutAttribute.Bottom.ToString()], attr, thisRatio);
+
+					if (cs.ContainsKey(NSLayoutAttribute.BottomMargin.ToString()))
+						UpdateConstraint(cs[NSLayoutAttribute.BottomMargin.ToString()], cvals[NSLayoutAttribute.BottomMargin.ToString()], attr, thisRatio);
+
 					break;
-				case MGAttribute.MGAttributeWidth:
-					UpdateConstraint(cs[NSLayoutAttribute.Width.ToString()], cvals[NSLayoutAttribute.Width.ToString()], attr, thisRatio);
+				case MGAttribute.Width:
+
+					if (cs.ContainsKey(NSLayoutAttribute.Width.ToString()))
+						UpdateConstraint(cs[NSLayoutAttribute.Width.ToString()], cvals[NSLayoutAttribute.Width.ToString()], attr, thisRatio);
+
 					break;
-				case MGAttribute.MGAttributeHeight:
-					UpdateConstraint(cs[NSLayoutAttribute.Height.ToString()], cvals[NSLayoutAttribute.Height.ToString()], attr, thisRatio);
+				case MGAttribute.Height:
+
+					if (cs.ContainsKey(NSLayoutAttribute.Height.ToString()))
+						UpdateConstraint(cs[NSLayoutAttribute.Height.ToString()], cvals[NSLayoutAttribute.Height.ToString()], attr, thisRatio);
+
 					break;
-				case MGAttribute.MGAttributeCornerRadius:
+				case MGAttribute.CornerRadius:
 					view.Layer.CornerRadius = attr.OrigValue + thisRatio * attr.Value;
 					break;
-				case MGAttribute.MGAttributeAlpha:
+				case MGAttribute.Alpha:
 					view.Alpha = attr.OrigValue + thisRatio * attr.Value;
 					break;
-				case MGAttribute.MGAttributeShadowRadius:
+				case MGAttribute.ShadowRadius:
 					view.Layer.ShadowRadius = attr.OrigValue + thisRatio * attr.Value;
 					break;
-				case MGAttribute.MGAttributeShadowOpacity:
+				case MGAttribute.ShadowOpacity:
 					view.Layer.ShadowOpacity = (float)(attr.OrigValue + thisRatio * attr.Value);
 					break;
-				case MGAttribute.MGAttributeFontSize:
+				case MGAttribute.FontSize:
 					if (view is UILabel) {
-						font = UIFont.FromName(((UILabel)view).Font.FamilyName, attr.OrigValue + thisRatio * attr.Value);
-						((UILabel)view).Font = font;
+						((UILabel)view).Font = ((UILabel)view).Font.WithSize(attr.OrigValue + thisRatio * attr.Value);
 					} else if (view is UIButton) {
-						font = UIFont.FromName(((UIButton)view).TitleLabel.Font.FamilyName, attr.OrigValue + thisRatio * attr.Value);
-						((UIButton)view).TitleLabel.Font = font;
+						((UIButton)view).Font = ((UIButton)view).Font.WithSize(attr.OrigValue + thisRatio * attr.Value);
 					} else if (view is UITextField) {
-						font = UIFont.FromName(((UITextField)view).Font.FamilyName, attr.OrigValue + thisRatio * attr.Value);
-						((UITextField)view).Font = font;
+						((UITextField)view).Font = ((UITextField)view).Font.WithSize(attr.OrigValue + thisRatio * attr.Value);
 					} else if (view is UITextView) {
-						font = UIFont.FromName(((UITextView)view).Font.FamilyName, attr.OrigValue + thisRatio * attr.Value);
-						((UITextView)view).Font = font;
+						((UITextView)view).Font = ((UITextView)view).Font.WithSize(attr.OrigValue + thisRatio * attr.Value);
 					}
 					break;
 			}
